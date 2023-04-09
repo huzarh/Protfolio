e9ad4cd97d1d7e2ea3a89efb6791773605dfb79e
khuzar <huzarh44@gmail.com>
2023-04-09 17:55:32 +0300

    f

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.listen(5004, () => {
+  console.log("Server is running on http://localhost:5004");
+});
